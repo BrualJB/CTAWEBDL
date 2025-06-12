5eb14ad26cdc895f417120c55ee8055dbaa71fb1
Emman <111825090+BrualJB@users.noreply.github.com>
2025-06-13 02:14:38 +0800

    Update ArticleListPage.jsx

--- Brual-front-end/src/pages/LandingPages/ArticleListPage.jsx
+++ Brual-front-end/src/pages/LandingPages/ArticleListPage.jsx
@@ -1,14 +1,25 @@
 import React from 'react';
-import ArticleList from '../../components/ArticleList';
-import articles from '../../article-content';
+import articles from '../../article-content'; // Adjust path if needed
+import { Link } from 'react-router-dom';
 
-function ArticleListPage() {
+function ArticleList() {
   return (
-    <>
+    <div className="article-list-container">
       <h1>Articles</h1>
-      <ArticleList articles={articles} />
-    </>
+      {articles.map((article) => (
+        <div className="article-preview" key={article.name}>
+          <h2>
+            <Link to={`/articles/${article.name}`}>{article.title}</Link>
+          </h2>
+          <p>
+            {Array.isArray(article.content) && typeof article.content[0] === 'string'
+              ? article.content[0].substring(0, 100) + '...'
+              : 'No preview available.'}
+          </p>
+        </div>
+      ))}
+    </div>
   );
 }
 
-export default ArticleListPage;
+export default ArticleList;
